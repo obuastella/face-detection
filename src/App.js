274ddaa7b68fba542d9a2d633b1ebbfb83a7da2e
Stella <stellaobua123@gmail.com>
2023-11-21 23:53:24 +0100

    Resolving git conflicts

--- src/App.js
+++ src/App.js
@@ -11,17 +11,57 @@
 // Add drawMesh to detect function
 
 import "./App.css";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import * as tf from "@tensorflow/tfjs";
 import * as facemesh from "@tensorflow-models/face-landmarks-detection";
 import Webcam from "react-webcam";
 // import { drawMesh } from "./utilities";
+import "@mediapipe/face_detection";
+import "@tensorflow/tfjs-core";
+// Register WebGL backend.
+import "@tensorflow/tfjs-backend-webgl";
+import * as faceDetection from "@tensorflow-models/face-detection";
 function App() {
   // set up references
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
 
   // Load Face mesh
+  const runFaceMesh = async () => {
+    // something here
+    const model = faceDetection.SupportedModels.MediaPipeFaceDetector;
+    const detectorConfig = {
+      runtime: "tfjs",
+    };
+    const detector = await facemesh.createDetector(model, detectorConfig);
+
+    // Detect function
+    const detect = async () => {
+      if (
+        typeof webcamRef.current !== undefined &&
+        webcamRef.current !== null &&
+        webcamRef.current.video.readyState !== 4
+      ) {
+        // Get video Properties
+        const video = webcamRef.current.video;
+        const videoWidth = webcamRef.current.video.videoWidth;
+        const videoHeight = webcamRef.current.video.videoHeight;
+        // set the video width
+        // added
+        webcamRef.current.video.videoWidth = videoWidthk;
+        webcamRef.current.video.videoHeight = videoHeight;
+        // set canvas properties
+        canvasRef.current.width = videoWidth;
+        canvasRef.current.height = videoHeight;
+      }
+      // make the detections
+      const face = await detector.estimateFaces(tf.image);
+      console.log(face);
+    };
+  };
+  useEffect(() => {
+    runFaceMesh();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
